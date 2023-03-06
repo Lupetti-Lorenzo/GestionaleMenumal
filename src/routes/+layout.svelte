e943980ab1df78bbe968607b548cc423c216d61c
Lupetti Lorenzo <lorenzo.lupetti3@gmail.com>
2023-03-06 23:18:06 +0100

    Implementata PWA con una vite-pwa/sveltekit e creato manifest.json che gli vada bene

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,37 @@
-<svelte:head>
-    <style>
-        body {
-            padding: 10px;
-        }
-    </style>
-</svelte:head>
 
 <script>
+///---------- PWA
+import { onMount } from 'svelte'
+import { pwaInfo } from 'virtual:pwa-info'
+
+let ReloadPrompt
+  
+onMount(async () => {
+    pwaInfo && (ReloadPrompt = (await import('$lib/components/reloadPrompt.svelte')).default)
+
+    if (pwaInfo) {
+        const { registerSW } = await import('virtual:pwa-register')
+        registerSW({
+        immediate: true,
+        onRegistered(r) {
+            // uncomment following code if you want check for updates
+            // r && setInterval(() => {
+            //    console.log('Checking for sw update')
+            //    r.update()
+            // }, 20000 /* 20s for testing purposes */)
+            console.log(`SW Registered: ${r}`)
+        },
+        onRegisterError(error) {
+            console.log('SW registration error', error)
+        }
+        })
+    }
+    })
+
+    $: webManifest = pwaInfo ? pwaInfo.webManifest.linkTag : ''
+    //--------------//
+
+
     import { authUser } from "$lib/client/auth"
     
     import  Nav  from "$lib/components/layout/nav.svelte"
@@ -16,10 +41,24 @@
 
 </script>
 
+<svelte:head>
+    {@html webManifest}
+
+    <style>
+        body {
+            padding: 10px;
+        }
+    </style>
+</svelte:head>
+
 <Nav />
 <!-- {#if logged}
     <p>userID: {user.id}</p>
     <p>mail: {user.email}</p>
 {/if} -->
+
+{#if ReloadPrompt}
+  <svelte:component this={ReloadPrompt} />
+{/if}
 
 <slot></slot>
