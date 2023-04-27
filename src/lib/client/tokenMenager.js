1db30d306496ac4b75bf7c2d07bbfbad1ec58618
Lupetti Lorenzo <lorenzo.lupetti3@gmail.com>
2023-04-27 20:47:14 +0200

    tokenmenager aggiustato e rimosso plugin pwa

--- src/lib/client/tokenMenager.js
+++ src/lib/client/tokenMenager.js
@@ -3,25 +3,24 @@ import { get } from 'svelte/store'
 import { readable } from 'svelte/store';
 
 async function getNewToken() {
+    if (get(authUser) == null) return ""
     const formData = new FormData();
-        formData.set('uid', get(authUser).id)
-        const res = await fetch("api/creaTokenMenumal", {
-            method: 'POST',
-            body: formData
-        });
-        const token = await res.json()
-        return token
+    formData.set('uid', get(authUser).id)
+    const res = await fetch("api/creaTokenMenumal", {
+        method: 'POST',
+        body: formData
+    });
+    const token = await res.json()
+    return token
 }
 
 // readable store che contiene il token di comunicazione con l'API di menumal, il token dura mezz'ora
 // dopo averlo inizialmente settato, avvio un timer che ogni mezz'ora lo aggiorna con uno nuovo
-export const token = readable("", async function start(set) {
-    set(await getNewToken()) 
-	const interval = setInterval(async () => {
-		set(await getNewToken());
+export const token = readable("",  function start(set) {
+    getNewToken().then(token => set(token))
+	const interval = setInterval(() => {
+		getNewToken().then(token => set(token))
 	}, 1790000); // ogni mezzora refresha il token
 
-	return function stop() {
-		clearInterval(interval);
-	};
+    return () => clearInterval(interval);
 });
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,46 +1,12 @@
 
 <script>
-///---------- PWA
-import { onMount } from 'svelte'
-import { pwaInfo } from 'virtual:pwa-info'
-
 //tailwind css
 import "../app.css";
 
-let ReloadPrompt
-  
-onMount(async () => {
-    pwaInfo && (ReloadPrompt = (await import('$lib/components/reloadPrompt.svelte')).default)
-
-    if (pwaInfo) {
-        const { registerSW } = await import('virtual:pwa-register')
-        registerSW({
-        immediate: true,
-        onRegistered(r) {
-            // uncomment following code if you want check for updates
-            // r && setInterval(() => {
-            //    console.log('Checking for sw update')
-            //    r.update()
-            // }, 20000 /* 20s for testing purposes */)
-            console.log(`SW Registered: ${r}`)
-        },
-        onRegisterError(error) {
-            console.log('SW registration error', error)
-        }
-        })
-    }
-    })
-
-    $: webManifest = pwaInfo ? pwaInfo.webManifest.linkTag : ''
-    //--------------//
-    
-    import  Nav  from "$lib/components/layout/nav.svelte"
-
+import  Nav  from "$lib/components/layout/nav.svelte"
 </script>
 
 <svelte:head>
-    {@html webManifest}
-
     <style>
         body {
             padding: 10px;
@@ -49,10 +15,5 @@ onMount(async () => {
 </svelte:head>
 
 <Nav />
-
-
-{#if ReloadPrompt}
-  <svelte:component this={ReloadPrompt} />
-{/if}
 
 <slot></slot>
