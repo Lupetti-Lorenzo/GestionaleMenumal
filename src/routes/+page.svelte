6327b274914c71362c07c9d3fc4a05bc103c57f7
Lupetti Lorenzo <lorenzo.lupetti3@gmail.com>
2023-05-04 22:55:48 +0200

    Controllo per nav che non compare: prova

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,8 @@
 	import JobsTable from "$lib/components/jobsTable.svelte"
 	import MenuSwitch from "$lib/components/menuSwitch.svelte"
 	import StatisticsTable from "../lib/components/statisticsView.svelte"
-	import { invalidateAll, invalidate } from "$app/navigation"
-	import { authUser } from "$lib/client/authStore"
+	import { invalidateAll } from "$app/navigation"
+	// import { authUser } from "$lib/client/authStore"
 
 	import { jobsStore, searchHandler } from "$lib/client/jobsStore.js"
 	import { onMount } from "svelte"
@@ -15,7 +15,7 @@
 		jobsStore.updateJobs()
 		console.log(JSON.stringify(data))
 		//if ($authUser == null) await invalidateAll() // controllo per refreshare quando user non autenticato, a volte non passa dalla load e non mostra la navbar
-		if (data == null) await invalidate() // controllo per refreshare quando user non autenticato, a volte non passa dalla load e non mostra la navbar
+		if (Object.keys(data).length === 0) await invalidateAll() // controllo per refreshare quando user non autenticato, a volte non passa dalla load e non mostra la navbar
 	})
 
 	// utilizzo questa sintassi per iscrivere un il search handler ogni volta che cambia jobStore
